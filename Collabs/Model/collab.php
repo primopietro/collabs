e1054f472ab932a9824f84d2bfe8901506c92225
primopietro <Pietro.hyde@gmail.com>
2017-07-27 11:00:40 -0400

    ok

---
+++ Collabs/Model/collab.php
@@ -0,0 +1,163 @@
+<?php
+class Collab {
+	protected $songName;
+	protected $artistName;
+	protected $isMain; 
+	protected $tableName;
+	function __construct() {
+		$this->songName= "";
+		$this->artistName= "";
+		$this->isMain = true;
+		$this->tableName = "collab";
+	}
+	function addToDatabase(){
+		include_once '../database_connect.php';
+		$boolean =1;
+		if($this->isMain != true){
+			$boolean =0;
+		}
+		
+		$sql =
+		"INSERT INTO `collab` (`songName`, `artistName`, `isMain`)
+		 VALUES ('$this->songName', '	$this->artistName', '$boolean')";
+	
+		if (! $result = $conn->query ( $sql )) {
+			echo "fail";
+			exit ();
+		} else {
+			echo "success";
+		}
+		
+	}
+	
+	function getObjectListFromDatabase(){
+		include_once '../database_connect.php';
+		$boolean =1;
+		if($this->isMain != true){
+			$boolean =2;
+		}
+		
+		$sql =
+		"INSERT INTO `collab` (`songName`, `artistName`, `isMain`)
+		VALUES ('$this->songName', '	$this->artistName', '$boolean')";
+		
+		if (! $result = $conn->query ( $sql )) {
+			echo "fail";
+			exit ();
+		} else {
+			echo "success";
+		}
+		
+	}
+
+	function getDBObjectListFromSongName($name){
+		include_once '../database_connect.php';
+		$name = htmlspecialchars($name);
+		$aCollabList = array();
+		$sql ="SELECT * FROM `collab`.`collab` WHERE songName = '$name' AND isMain = 1 ";
+		
+		$result = $conn->query ( $sql );
+		
+		if ($result->num_rows > 0) {
+			while ( $row = $result->fetch_assoc () ) {
+				$aCollab = array();
+				$aCollab['artistName'] = $row["artistName"];
+				$aCollabList[$row["songName"]] = $aCollab;
+			}
+		}
+		return $aCollabList;
+	}
+	
+	function getDBObjectJSONFromSongName($name){
+		
+		return json_encode($this->getDBObjectListFromSongName($name));
+	}
+	
+	function getDBObjectListFromArtistNames($artists){
+		include_once '../database_connect.php';
+		$aCollabList = array();
+		$sql ="SELECT songName ";
+		$compter = 0;
+		foreach ($artists as &$artist) {
+			$sql .=" FROM collab as c$compter
+			WHERE c$compter.artistName LIKE '%$artist%'";
+			$compter++;
+			if($compter < sizeOf($artists)){
+				$sql .=" UNION SELECT songName ";
+			}
+		}
+		
+		$sql .=" GROUP BY songName";
+		//echo $sql;
+		$result = $conn->query ( $sql );
+		
+		if ($result->num_rows > 0) {
+			$fastechObjects = array ();
+			while ( $row = $result->fetch_assoc () ) {			
+				$aCollabList[] =  $row["songName"];
+			}
+		}
+		return $aCollabList;
+	}
+	function getDBObjectJSONFromArtistNames($artists){
+		echo json_encode($this->getDBObjectListFromArtistNames($artists));
+	}
+    /**
+     * songName
+     * @return unkown
+     */
+    public function getSongName(){
+        return $this->songName;
+    }
+
+    /**
+     * songName
+     * @param unkown $songName
+     * @return Collab
+     */
+    public function setSongName($songName){
+        $this->songName = $songName;
+        return $this;
+    }
+
+    /**
+     * artistName
+     * @return unkown
+     */
+    public function getArtistName(){
+        return $this->artistName;
+    }
+
+    /**
+     * artistName
+     * @param unkown $artistName
+     * @return Collab
+     */
+    public function setArtistName($artistName){
+        $this->artistName = $artistName;
+        return $this;
+    }
+
+    /**
+     * isMain
+     * @return unkown
+     */
+    public function getIsMain(){
+        return $this->isMain;
+    }
+
+    /**
+     * isMain
+     * @param unkown $isMain
+     * @return Collab
+     */
+    public function setIsMain($isMain){
+        $this->isMain = $isMain;
+        return $this;
+    }
+
+}
+
+$aCollab = new Collab();
+echo $aCollab->getDBObjectJSONFromArtistNames(["Childish Gambino","Kauai","Josh"]);
+
---
+++ Collabs/create_database_environement.php
@@ -0,0 +1,88 @@
+<?php
+/***********************************Connect to datase environement*****************************************/
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'collab';
+$conn = new mysqli($dbhost, $dbuser, $dbpass);
+
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+else{
+	echo 'Connected successfully';
+}
+echo "<br>";
+/******************************************Delete old environement***************************************/
+$sql = 'DROP DATABASE ' . $dbname ;
+if (!$result = $conn->query($sql)) {
+	echo "<span style='color:red;'>Could not drop database " . $dbname . "</span>";
+	
+}
+else{
+	echo "<span style='color:green;'>Database " . $dbname . " deleted successfully</span>\n";
+}
+echo "<br>";
+/****************************************Create new environement*************************************/
+
+$sql = 'CREATE DATABASE ' . $dbname;
+if (!$result = $conn->query($sql)) {
+	// Oh no! The query failed.
+	echo "<span style='color:red;'>Could not create database " . $dbname . "</span>";
+	exit;
+}
+else{
+	echo "<span style='color:green;'>Database " . $dbname . " created successfully</span>\n";
+}
+echo "<br>";
+/*******************************************CONNECT TO DATABASE*************************************/
+$conn->close();
+$conn = null;
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+	echo "<span style='color:red;'>Connection failed" . $dbname . "</span>";
+}
+
+else{
+	echo '<span color="green"> Connected successfully to ' .  $dbname . "</span>";
+}
+echo "<br>";
+
+/***************************Database structuring (tables, primary keys, etc.)***********************/
+
+$sql = 'CREATE TABLE `collab`.`collab` (
+  `songName` varchar(255) NOT NULL,
+  `artistName` varchar(255) NOT NULL,
+  `isMain` boolean NOT NULL
+) ENGINE=InnoDB ';
+if (!$result = $conn->query($sql)) {
+	// Oh no! The query failed.
+	echo "<span style='color:red;'>Could not create table collab</span>" ;
+	exit;
+}
+else{
+	echo "<span style='color:green;'>Table collab created successfully</span>\n";
+}
+echo "<br>";
+
+
+$sql = "INSERT INTO `collab` (`songName`, `artistName`, `isMain`) VALUES ('Giants', 'Childish Gambino', true), ('Giants', 'Josh Osho', false), ('Giants', 'Kauai', false);
+";
+if (!$result = $conn->query($sql)) {
+	// Oh no! The query failed.
+	echo "<span style='color:red;'>Could not insert into table collab</span>" ;
+	exit;
+}
+else{
+	echo "<span style='color:green;'Insert successfull</span>\n";
+}
+echo "<br>";
+
+
+/**********************************************Close db connection**********************************/
+$conn->close();
+?>
