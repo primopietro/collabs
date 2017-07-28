c63543c291c560a847f62828ab0507163c0a53fc
primopietro <Pietro.hyde@gmail.com>
2017-07-28 00:34:43 -0400

    fixed

--- Collabs/dataFeeder/dataFeeder.php
+++ Collabs/dataFeeder/dataFeeder.php
@@ -1,9 +1,10 @@
 <?php
 include_once '../model/collab.php';
 $str = file_get_contents('basicArtistList.json');
-$json = json_decode($str, true);
-$aCollab = new Collab();
 
+$json = json_decode( preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $str), true );
+
+$aCollab = new Collab();
 
 foreach ($json as $key => $value) {
 	
